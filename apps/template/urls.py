c491ddadf22722669fd2a0f9160e38f5705532c8
Alagusundaram Chockalingam <alagusundaram.chockalingam@impigertech.com>
2026-02-08 17:46:17 +0530

    Added template view

---
+++ apps/template/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.template import views
+
+urlpatterns = [
+    path('', views.billing_form, name='billing-form'),
+    path('bill/<str:order_code>/', views.billing_result, name='billing-result'),
+]
--- apps/template/views.py
+++ apps/template/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from apps.billing.models import PurchaseOrder
+from core.settings import VALID_DENOMINATIONS
+
+
+def billing_form(request):
+    denominations = sorted(VALID_DENOMINATIONS, reverse=True)
+    return render(request, 'billing/billing_form.html', {'denominations': denominations})
+
+
+def billing_result(request, order_code):
+    order = get_object_or_404(PurchaseOrder, code=order_code, is_draft=False)
+    items = order.purchase_items.select_related('product').all()
+    change_details = order.denomination_details.filter(
+        type='balance'
+    ).select_related('denomination')
+
+    for item in items:
+        item.subtotal = item.get_subtotal()
+        item.tax_amount = item.get_tax_amount()
+        item.total = item.get_total()
+
+    return render(request, 'billing/billing_result.html', {
+        'order': order,
+        'items': items,
+        'change_details': change_details,
+    })
